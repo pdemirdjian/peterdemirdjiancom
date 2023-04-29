17b19c8e4fe8e9f2597171e4fd37eadaf5509d95
Pete Demirdjian <pdemirdjian@users.noreply.github.com>
2023-04-29 10:42:09 -0400

    pnpm is legitness

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,8 @@
+import { defineUserConfig } from 'vuepress'
 import { defaultTheme } from '@vuepress/theme-default'
 import { googleAnalyticsPlugin } from '@vuepress/plugin-google-analytics'
 
-export default {
+export default defineUserConfig({
   lang: 'en-US',
   title: 'Pete Demirdjian',
   description: 'Made by Pete Demirdjian with ❤️',
@@ -27,11 +28,12 @@ export default {
         text: 'Email',
         link: 'mailto:code@peterdemirdjian.com'
       }
-    ]
+    ],
+    repo: 'pdemirdjian/peterdemirdjiancom'
   }),
   plugins: [
     googleAnalyticsPlugin({
       id: 'G-8MJYD301PV',
     }),
   ],
-}
+})

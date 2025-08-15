5f4a0527290c8ff2c09043c12c78c11d1c017cbe
Pete Demirdjian <3103454+pdemirdjian@users.noreply.github.com>
2025-08-15 00:43:11 -0400

    Create eslint.yml (#307)
    * Create eslint.yml
    
    * fixing
    
    * eslint config
    
    * yerp
    
    * fixing action

---
+++ eslint.config.js
@@ -0,0 +1,99 @@
+import js from '@eslint/js'
+
+export default [
+  // Global ignores - must be the first item
+  {
+    ignores: [
+      'node_modules/**',
+      'dist/**',
+      'docs/.vuepress/dist/**',
+      'docs/.vuepress/.cache/**',
+      'docs/.vuepress/.temp/**',
+      '**/*.min.js',
+      '**/*.d.ts',
+      'pnpm-lock.yaml',
+      '**/*.log',
+      '.env*',
+    ],
+  },
+
+  // Base configuration for JavaScript files
+  {
+    files: ['**/*.js', '**/*.mjs'],
+    ...js.configs.recommended,
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+        Buffer: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+        module: 'readonly',
+        require: 'readonly',
+        exports: 'readonly',
+        global: 'readonly',
+        window: 'readonly',
+        document: 'readonly',
+      },
+    },
+    rules: {
+      'no-console': 'warn',
+      'no-debugger': 'error',
+      'prefer-const': 'error',
+      'no-var': 'error',
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      'indent': ['error', 2],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'never'],
+      'comma-dangle': ['error', 'always-multiline'],
+    },
+  },
+
+  // TypeScript files
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+        Buffer: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+        module: 'readonly',
+        require: 'readonly',
+        exports: 'readonly',
+        global: 'readonly',
+        window: 'readonly',
+        document: 'readonly',
+        setTimeout: 'readonly',
+        clearTimeout: 'readonly',
+        setInterval: 'readonly',
+        clearInterval: 'readonly',
+      },
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      'no-console': 'warn',
+      'no-debugger': 'error',
+      'prefer-const': 'error',
+      'no-var': 'error',
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      'indent': ['error', 2],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'never'],
+      'comma-dangle': ['error', 'always-multiline'],
+    },
+  },
+
+  // Special rules for config files
+  {
+    files: ['docs/.vuepress/config.js'],
+    rules: {
+      'no-console': 'off',
+    },
+  },
+]

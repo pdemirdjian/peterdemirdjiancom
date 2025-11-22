a5ed5ccb6f9b0d6070e56efb367b6647a1e3d834
Pete Demirdjian <pdemirdjian@gmail.com>
2025-11-21 22:35:20 -0500

    updates everywhere

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -1,7 +1,11 @@
 import { defineClientConfig } from '@vuepress/client'
 
 export default defineClientConfig({
-  enhance({ app: _app, router, siteData: _siteData }) {
+  enhance({ app, router, siteData }) {
+    // Mark unused parameters explicitly
+    void app
+    void siteData
+
     if (typeof window !== 'undefined') {
       // Use router to update years when page changes
       router.afterEach(() => {
